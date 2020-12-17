974e318156e46640a1cfa0d04f4efa57d4f5aae8
Meirylindi <meirylindimlima@gmail.com>
2020-12-17 14:17:42 +0000

    Atividade concluída.

--- main.py
+++ main.py
@@ -3,7 +3,17 @@
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
-    # COLOQUE SEU CÓDIGO AQUI
+    print('Digite um número:')
+    n_1 = float(input('> '))
+    print('Digite outro número:')
+    n_2 = float(input('> '))
+
+    if n_1 > n_2:
+      maior = n_1
+    else:
+      maior = n_2
+
+    print(f'{maior}')
 
 if __name__ == '__main__':
     main()
